3296647265ebdc26ddc940aec23260b0f891758f
aniimessh <animeshgupta2.002@gmail.com>
2024-07-07 18:29:27 +0530

    feat: herosection and navbar finished

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,20 +1,22 @@
 import Image from "next/image";
 import HeroImage from "../../public/bgimage.png";
 import HeroGif from "../../public/academy-animated-logo-57b2ae61.gif";
+import { FiArrowRight } from "react-icons/fi";
 
 const HeroSection = () => {
   return (
     <div>
-      <Image src={HeroImage} alt="heroimagei" className="pt-4 relative" />
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 mix-blend-screen text-center flex flex-col items-center">
+      <Image src={HeroImage} alt="heroimagei" className="relative w-full"  />
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 mix-blend-screen text-center flex flex-col gap-y-6 items-center">
         <Image src={HeroGif} alt="herotext" className="" height={100} />
-        <p className="w-2/3 font-medium text-lg">
+        <p className="w-3/4 font-medium text-lg text-white">
           Intract users{" "}
           <span className="text-[#454345]"> have together made more than</span>{" "}
           $100 million <span className="text-[#454345]">in web3</span>.{" "}
           <span className="text-[#454345]">Join them</span> and learn how to
           earn crypto!
         </p>
+        <button className="bg-[#7242EC] bg-opacity-80 px-11 py-1 rounded-md text-sm flex items-center gap-1 text-white">Get Started <FiArrowRight /></button>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,11 @@ const Navbar = () => {
   const [active, setActive] = useState("Academy");
 
   return (
-    <div className="w-full border-b fixed bg-black py-3 z-10">
+    <div className="w-full border-b-[#ffffff34] border-b fixed bg-black py-3 z-10">
       <div className="flex w-5/6 mx-auto items-center justify-between">
         <div className="flex items-center gap-x-6">
           <Image src={Logo} height={100} width={100} alt="logo" />
-          <nav className="flex gap-x-6 flex-1">
+          <nav className="flex gap-x-6 flex-1 text-white">
             <Link href={""} className="opacity-80 hover:opacity-100">
               Compass
             </Link>
@@ -43,17 +43,14 @@ const Navbar = () => {
         <div className="flex items-center gap-x-3 justify-end">
           <div className="relative">
             <IoSearchOutline
-              className="absolute top-1/2 -translate-y-1/2 left-2 opacity-40"
+              className="absolute top-1/2 -translate-y-1/2 left-2 opacity-40 text-gray-600"
               fontSize="24"
             />
             <input
               type="text"
               placeholder="Search for ecosystem, trending quests etc.."
-              className="w-full py-2 pl-10 pr-3 bg-[rgba(255,255,255,0.07)] rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:opacity-40"
+              className="w-full py-2 pl-10 pr-3 bg-[rgba(255,255,255,0.07)] rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:opacity-40"
             />
-          </div>
-          <div className="bg-[rgba(255,255,255,0.07)] p-2 rounded-full">
-            <IoSearchOutline fontSize="24" />
           </div>
           <div className="bg-[rgba(255,255,255,0.07)] p-2 rounded-full border border-[#9D591B]">
             <Image src={FeedIcon} height={20} width={20} alt="icon"></Image>
